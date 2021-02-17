7cdeb2b2573a4bb8da43f27a2f32c43a2d9bfe5c
quiztech <quiztechoverdrive@gmail.com>
2021-02-17 15:13:05 +0900

    四捨五入テストを成功するよう戻した

--- test/test.js
+++ test/test.js
@@ -25,7 +25,7 @@ describe('#effectiveDamage()', () => {
   });
 
   it('ダメージは小数点以下を四捨五入して整数にする', () => {
-    assert.equal(dc.effectiveDamage(620, 100, 30), 364);
+    assert.equal(dc.effectiveDamage(620, 100, 30), 365);
   });
 
 });
